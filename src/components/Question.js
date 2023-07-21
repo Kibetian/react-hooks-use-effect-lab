6b6d7198c48f8663aea8cf3a80164582e6934012
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-07-21 14:27:05 +0300

    effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  const decreaseTimeRemaining = () => {
+    setTimeRemaining((prevTime) => prevTime -1);
+  }
 
   // add useEffect code
+  useEffect(() => {
+    const timerID =setTimeout(() => {
+      decreaseTimeRemaining();
+    }, 1000)
+    return() => clearTimeout(timerID)
+  },[timeRemaining]
+)
+
+   useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false)
+    }
+   }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
